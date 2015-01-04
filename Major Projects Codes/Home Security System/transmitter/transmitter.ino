f3b48b97181fd9425b3ffe0d47fcc27cc1e86451
celab2010 <celab2010@gmail.com>
2015-01-04 06:33:47 +0000

    ok

--- Major Projects Codes/Home Security System/transmitter/transmitter.ino
+++ Major Projects Codes/Home Security System/transmitter/transmitter.ino
@@ -37,29 +37,30 @@ int flag=0;
 //char msg[7] = {'H','E','L','L','O','O','K'};
 char msg[128];
 char incomingByte = 0;   // for incoming serial data
-uint8_t buf[VW_MAX_MESSAGE_LEN];
-uint8_t buflen = VW_MAX_MESSAGE_LEN;
 
 void loop()
 {
+uint8_t buf[VW_MAX_MESSAGE_LEN];
+uint8_t buflen = VW_MAX_MESSAGE_LEN;
+        //Serial.print("Wait:");
+        if (vw_get_message(buf, &buflen)) // Non-blocking
+        {
+          buf[buflen] = '\n';
+          Serial.println((char *)buf);
+          //delay(1000);
+        }
+
         while(Serial.available() > 0) {
                 // read the incoming byte:
                 incomingByte = Serial.read();
                msg[count++]=incomingByte;
                flag=1;
-               
-        }
-        
-        if (vw_get_message(buf, &buflen)) // Non-blocking
-        {
-          buf[buflen] = '\n';
-          Serial.println((char *)buf);
         }
 
         msg[count]='\0';
         if(flag == 1)
         {
-          Serial.println(msg);
+          //Serial.println(msg);
           //Sending the string
           vw_send((uint8_t *)msg, strlen(msg));
           vw_wait_tx(); 
