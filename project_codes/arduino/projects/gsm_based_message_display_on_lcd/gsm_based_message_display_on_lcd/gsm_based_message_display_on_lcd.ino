fb1fa36264df489902932a53af2fb51f3b3cece6
rohit57singh <rohit57singh@gmail.com>
2015-03-14 09:33:19 +0000

    add

---
+++ project_codes/arduino/projects/gsm_based_message_display_on_lcd/gsm_based_message_display_on_lcd/gsm_based_message_display_on_lcd.ino
@@ -0,0 +1,68 @@
+ 
+#include <SoftwareSerial.h>
+#include <LiquidCrystal.h>
+SoftwareSerial SIM900(4, 5); //tx-4 rx-5
+LiquidCrystal lcd(13, 12, 11, 10, 9, 8);
+char input[36];
+int count = 0; // A variable to count the length of the Tag DATA
+
+
+char incoming_char=0;
+ 
+void setup()
+{
+  // set up the LCD's number of columns and rows: 
+  lcd.begin(16, 2);
+   lcd.print("WELCOME");
+  Serial.begin(9600); // for serial monitor
+  SIM900.begin(19200); // for GSM shield
+  SIM900power();  // turn on shield
+  delay(20000);  // give time to log on to network.
+ 
+  SIM900.print("AT+CMGF=1\r");  // set SMS mode to text
+  delay(100);
+  SIM900.print("AT+CNMI=2,2,0,0,0\r"); 
+  // blurt out contents of new SMS upon receipt to the GSM shield's serial out
+  delay(100);
+     for(int i=0;i<36;i++)
+        input[i]='\0';
+          lcd.setCursor(0, 1);
+          count=0;
+}
+ 
+void SIM900power()
+// software equivalent of pressing the GSM shield "power" button
+{
+  digitalWrite(9, HIGH);
+  delay(1000);
+  digitalWrite(9, LOW);
+  delay(7000);
+}
+ 
+void loop()
+{
+  // set the cursor to column 0, line 1
+  // (note: line 1 is the second row, since counting begins with 0):
+
+  // Now we simply display any text that the GSM shield sends out on the serial monitor
+  if(SIM900.available() >0)
+  {
+
+    incoming_char=SIM900.read(); //Get the character from the cellular serial port.
+    Serial.print(incoming_char); //Print the incoming character to the terminal.
+    input[count] = incoming_char; // Read 1 Byte of data and store it in a character variable
+    count++; // Increment the Byte count after every Byte Read
+    //delay(5); 
+
+  }
+
+   if(count > 35)
+      {
+        count=0;
+        for(int i=0;i<36;i++)
+        input[i]='\0';
+      }
+      lcd.clear();
+            lcd.print(input);
+  
+}
