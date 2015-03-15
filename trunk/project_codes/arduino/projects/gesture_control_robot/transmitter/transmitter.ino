0ab93e14246f89e63fbe47793e1c8daf0ee55b64
pandit4india@gmail.com <pandit4india@gmail.com@cec4852c-2b8b-980c-9b29-aebe1ef4e3ee>
2015-03-15 11:21:30 +0000

    Transmitter code complete

--- trunk/project_codes/arduino/projects/gesture_control_robot/transmitter/transmitter.ino
+++ trunk/project_codes/arduino/projects/gesture_control_robot/transmitter/transmitter.ino
@@ -1,6 +1,6 @@
 #include <LiquidCrystal.h>
 #include <VirtualWire.h>
-const int transmit_pin = 1;
+const int transmit_pin = 2;
 const int transmit_en_pin = 3;
 LiquidCrystal lcd(13, 12, 11, 10, 9, 8);
 
@@ -14,15 +14,16 @@ int z_value = 340;
 void setup()
 {
   // initialize the serial communications:
-  Serial.begin(9600);
  vw_set_tx_pin(transmit_pin);
  vw_set_ptt_pin(transmit_en_pin);
  vw_set_ptt_inverted(true); // Required for DR3100
   vw_setup(2000);       // Bits per sec
   lcd.begin(16, 2);
-  lcd.print("WELCOME RRS");
+  lcd.print("GESTURE CONTROL");
   lcd.setCursor(0, 0);
-
+  lcd.setCursor(0, 1);
+   lcd.print("   ROBOT ! ");
+  delay(1000);
  
 }
 
@@ -31,78 +32,71 @@ void loop()
   int x = analogRead(xpin);
   int y = analogRead(ypin);
   int z = analogRead(zpin);
-  // print the sensor values:
-  Serial.print(x);
-  Serial.print(",");
-  Serial.print(y);
-  Serial.print(",");
-  Serial.print(z);
-  Serial.println();
+
   if(abs(x - x_value) >= 20 && x > x_value)
   {
-    lcd.clear();
-    lcd.print("forward");
-    fwd();
+    rgt();
   }
   else if(abs(x - x_value) >= 20 && x < x_value)
   {
-    lcd.clear();
-    lcd.print("backward");
-    bwk();
+    lft();
   }
   else if(abs(y - y_value) >= 20 && y > y_value)
   {
-    lcd.clear();
-    lcd.print("left");
-    lft();
+    fwd();
   }
   else if(abs(y - y_value) >= 20 && y < y_value)
   {
-    lcd.clear();
-    lcd.print("right");
-    rgt();
+     bwk();
   }
   else
   {
-    lcd.clear();
-    lcd.print("stop");
     stp();
   }
   delay(100);
-
-}
+  }
 
 void fwd()
 { 
-   char msg[] = {'f'};
+      lcd.clear();
+    lcd.print("forward");
+   char msg[] = {'f','\0'};
    vw_send((uint8_t *)msg, 2);
   vw_wait_tx(); // Wait until the whole message is gone
   delay(300);
 }
 void bwk()
 { 
-   char msg[] = {'b'};
+      lcd.clear();
+    lcd.print("backward");
+   char msg[] = {'b','\0'};
    vw_send((uint8_t *)msg, 2);
   vw_wait_tx(); // Wait until the whole message is gone
   delay(300);
 }
 void lft()
 { 
-   char msg[] = {'l'};
+      lcd.clear();
+    lcd.print("left");
+   char msg[] = {'l','\0'};
    vw_send((uint8_t *)msg, 2);
   vw_wait_tx(); // Wait until the whole message is gone
   delay(300);
 }
 void rgt()
 { 
-   char msg[] = {'r'};
+      lcd.clear();
+    lcd.print("right");
+   char msg[] = {'r','\0'};
    vw_send((uint8_t *)msg, 2);
   vw_wait_tx(); // Wait until the whole message is gone
   delay(300);
 }
 void stp()
 { 
-   char msg[] = {'s'};
+      lcd.clear();
+    lcd.print("stop");
+   char msg[] = {'s','\0'};
    vw_send((uint8_t *)msg, 2);
   vw_wait_tx(); // Wait until the whole message is gone
   delay(300);
