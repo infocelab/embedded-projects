fc7b8bf0868e1b282d4fe9e6a68c6cc3f418d19a
pandit4india@gmail.com <pandit4india@gmail.com@cec4852c-2b8b-980c-9b29-aebe1ef4e3ee>
2015-03-18 11:38:51 +0000

    updated

--- trunk/project_codes/arduino/projects/gesture_control_robot/transmitter/transmitter.ino
+++ trunk/project_codes/arduino/projects/gesture_control_robot/transmitter/transmitter.ino
@@ -20,11 +20,19 @@ void setup()
   vw_setup(2000);       // Bits per sec
   lcd.begin(16, 2);
   lcd.print("GESTURE CONTROL");
-  lcd.setCursor(0, 0);
+
   lcd.setCursor(0, 1);
-   lcd.print("   ROBOT!  ");
+   lcd.print("ROBOT By-Nishant");
   delay(1000);
  
+}
+void print_on_lcd(int x, int y , int z)
+{
+    lcd.setCursor(0, 1);
+    lcd.print("x=");
+    lcd.print(x);
+    lcd.print(",Y=");
+    lcd.print(y);     
 }
 
 void loop()
@@ -36,22 +44,27 @@ void loop()
   if(abs(x - x_value) >= 20 && x > x_value)
   {
     rgt();
+    print_on_lcd(x,y,z);
   }
   else if(abs(x - x_value) >= 20 && x < x_value)
   {
     lft();
+        print_on_lcd(x,y,z);
   }
   else if(abs(y - y_value) >= 20 && y > y_value)
   {
     fwd();
+        print_on_lcd(x,y,z);
   }
   else if(abs(y - y_value) >= 20 && y < y_value)
   {
      bwk();
+         print_on_lcd(x,y,z);
   }
   else
   {
     stp();
+        print_on_lcd(x,y,z);
   }
   delay(100);
   }
@@ -59,6 +72,7 @@ void loop()
 void fwd()
 { 
       lcd.clear();
+    lcd.setCursor(0, 0);
     lcd.print("forward");
    char msg[] = {'f','\0'};
    vw_send((uint8_t *)msg, 2);
@@ -68,6 +82,7 @@ void fwd()
 void bwk()
 { 
       lcd.clear();
+        lcd.setCursor(0, 0);
     lcd.print("backward");
    char msg[] = {'b','\0'};
    vw_send((uint8_t *)msg, 2);
@@ -77,6 +92,7 @@ void bwk()
 void lft()
 { 
       lcd.clear();
+        lcd.setCursor(0, 0);
     lcd.print("left");
    char msg[] = {'l','\0'};
    vw_send((uint8_t *)msg, 2);
@@ -86,6 +102,7 @@ void lft()
 void rgt()
 { 
       lcd.clear();
+        lcd.setCursor(0, 0);
     lcd.print("right");
    char msg[] = {'r','\0'};
    vw_send((uint8_t *)msg, 2);
@@ -95,6 +112,7 @@ void rgt()
 void stp()
 { 
       lcd.clear();
+        lcd.setCursor(0, 0);
     lcd.print("stop");
    char msg[] = {'s','\0'};
    vw_send((uint8_t *)msg, 2);
