368c86a46d7f6b91b05dc91185059bf125c0ee84
rohit57singh <rohit57singh@gmail.com>
2015-03-18 13:00:06 +0000

    four_relay_channel

---
+++ project_codes/pic/four_relay_channel/four_relay_channel.asm
@@ -0,0 +1,158 @@
+
+_main:
+
+;four_relay_channel.c,1 :: 		void main()
+;four_relay_channel.c,3 :: 		trisb=0x00;
+	CLRF       TRISB+0
+;four_relay_channel.c,4 :: 		while(1)
+L_main0:
+;four_relay_channel.c,6 :: 		portb.rb0=0x00;
+	BCF        PORTB+0, 0
+;four_relay_channel.c,7 :: 		delay_ms(500);
+	MOVLW      3
+	MOVWF      R11+0
+	MOVLW      138
+	MOVWF      R12+0
+	MOVLW      85
+	MOVWF      R13+0
+L_main2:
+	DECFSZ     R13+0, 1
+	GOTO       L_main2
+	DECFSZ     R12+0, 1
+	GOTO       L_main2
+	DECFSZ     R11+0, 1
+	GOTO       L_main2
+	NOP
+	NOP
+;four_relay_channel.c,8 :: 		portb.rb0=0xff;
+	BSF        PORTB+0, 0
+;four_relay_channel.c,9 :: 		delay_ms(500);
+	MOVLW      3
+	MOVWF      R11+0
+	MOVLW      138
+	MOVWF      R12+0
+	MOVLW      85
+	MOVWF      R13+0
+L_main3:
+	DECFSZ     R13+0, 1
+	GOTO       L_main3
+	DECFSZ     R12+0, 1
+	GOTO       L_main3
+	DECFSZ     R11+0, 1
+	GOTO       L_main3
+	NOP
+	NOP
+;four_relay_channel.c,10 :: 		portb.rb1=0x00;
+	BCF        PORTB+0, 1
+;four_relay_channel.c,11 :: 		delay_ms(500);
+	MOVLW      3
+	MOVWF      R11+0
+	MOVLW      138
+	MOVWF      R12+0
+	MOVLW      85
+	MOVWF      R13+0
+L_main4:
+	DECFSZ     R13+0, 1
+	GOTO       L_main4
+	DECFSZ     R12+0, 1
+	GOTO       L_main4
+	DECFSZ     R11+0, 1
+	GOTO       L_main4
+	NOP
+	NOP
+;four_relay_channel.c,12 :: 		portb.rb1=0xff;
+	BSF        PORTB+0, 1
+;four_relay_channel.c,13 :: 		delay_ms(500);
+	MOVLW      3
+	MOVWF      R11+0
+	MOVLW      138
+	MOVWF      R12+0
+	MOVLW      85
+	MOVWF      R13+0
+L_main5:
+	DECFSZ     R13+0, 1
+	GOTO       L_main5
+	DECFSZ     R12+0, 1
+	GOTO       L_main5
+	DECFSZ     R11+0, 1
+	GOTO       L_main5
+	NOP
+	NOP
+;four_relay_channel.c,14 :: 		portb.rb2=0x00;
+	BCF        PORTB+0, 2
+;four_relay_channel.c,15 :: 		delay_ms(500);
+	MOVLW      3
+	MOVWF      R11+0
+	MOVLW      138
+	MOVWF      R12+0
+	MOVLW      85
+	MOVWF      R13+0
+L_main6:
+	DECFSZ     R13+0, 1
+	GOTO       L_main6
+	DECFSZ     R12+0, 1
+	GOTO       L_main6
+	DECFSZ     R11+0, 1
+	GOTO       L_main6
+	NOP
+	NOP
+;four_relay_channel.c,16 :: 		portb.rb2=0xff;
+	BSF        PORTB+0, 2
+;four_relay_channel.c,17 :: 		delay_ms(500);
+	MOVLW      3
+	MOVWF      R11+0
+	MOVLW      138
+	MOVWF      R12+0
+	MOVLW      85
+	MOVWF      R13+0
+L_main7:
+	DECFSZ     R13+0, 1
+	GOTO       L_main7
+	DECFSZ     R12+0, 1
+	GOTO       L_main7
+	DECFSZ     R11+0, 1
+	GOTO       L_main7
+	NOP
+	NOP
+;four_relay_channel.c,18 :: 		portb.rb3=0x00;
+	BCF        PORTB+0, 3
+;four_relay_channel.c,19 :: 		delay_ms(500);
+	MOVLW      3
+	MOVWF      R11+0
+	MOVLW      138
+	MOVWF      R12+0
+	MOVLW      85
+	MOVWF      R13+0
+L_main8:
+	DECFSZ     R13+0, 1
+	GOTO       L_main8
+	DECFSZ     R12+0, 1
+	GOTO       L_main8
+	DECFSZ     R11+0, 1
+	GOTO       L_main8
+	NOP
+	NOP
+;four_relay_channel.c,20 :: 		portb.rb4=0xff;
+	BSF        PORTB+0, 4
+;four_relay_channel.c,21 :: 		delay_ms(500);
+	MOVLW      3
+	MOVWF      R11+0
+	MOVLW      138
+	MOVWF      R12+0
+	MOVLW      85
+	MOVWF      R13+0
+L_main9:
+	DECFSZ     R13+0, 1
+	GOTO       L_main9
+	DECFSZ     R12+0, 1
+	GOTO       L_main9
+	DECFSZ     R11+0, 1
+	GOTO       L_main9
+	NOP
+	NOP
+;four_relay_channel.c,22 :: 		}
+	GOTO       L_main0
+;four_relay_channel.c,24 :: 		}
+L_end_main:
+	GOTO       $+0
+; end of _main
