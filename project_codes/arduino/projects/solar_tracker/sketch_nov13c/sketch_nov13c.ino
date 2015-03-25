1ad58d07e17c8a96d319a2674d16fb69b9572081
pandit4india <pandit4india@gmail.com>
2015-03-25 15:00:59 +0000

    modified

--- project_codes/arduino/projects/solar_tracker/sketch_nov13c/sketch_nov13c.ino
+++ project_codes/arduino/projects/solar_tracker/sketch_nov13c/sketch_nov13c.ino
@@ -1,30 +1,15 @@
-/* 
- Stepper Motor example
-  
- This program drives a bipolar or unipolar stepper motor. 
- The motor is attached to digital pins 2 - 5 of the Arduino.
-  
- If the button attached to A0 is pressed the motor should revolve one revolution in one direction,
- while if the button attached to A1 is pressed then one revolution in the other direction.  
-  
- Author: Arturo Guadalupi <a.guadalupi@arduino.cc> 
- */
  
 #include <Stepper.h>
  int leftsensor = A0; 
  int rightsensor = A1; 
- 
+ int val_diff = 100;
 const int stepsPerRevolution = 20;  // change this to fit the number of steps per revolution
-// for your motor
- 
-// initialize the stepper library on pins 2 through 5:
-int stepCount = 0;
+
   Stepper myStepper(stepsPerRevolution, 9,10,11,12);            
  
 void setup() 
 {
   Serial.begin(9600);
-  // set the speed at 10 rpm:
   myStepper.setSpeed(60);
   pinMode(leftsensor, INPUT);
   pinMode(rightsensor, INPUT);
@@ -41,19 +26,17 @@ void loop()
   Serial.print(left);
   Serial.print("     Right Sensor:");
   Serial.println(analogRead(rightsensor));
-
-  delay(1000);
-  if ( abs(left - right) > 30 && left > right)
+  if ( abs(left - right) > val_diff && left > right)
   {  
      Serial.println("moving left");
    myStepper.step(-stepsPerRevolution);
   }
-  else if ( abs(left - right) > 30 && right > left)
+  else if ( abs(left - right) > val_diff && right > left)
   {
      Serial.println("moving right");
     myStepper.step(stepsPerRevolution);
   }
-     delay(500);  
+  delay(10000);  
 }
  
 
