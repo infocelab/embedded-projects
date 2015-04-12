eb1fdd763e5d7b88e41dc5ce2e583eac01bd90fb
pandit4india <pandit4india@gmail.com>
2015-04-12 10:10:01 +0000

    code added

---
+++ micro_controllers/arduino/projects/relay_on_off/relay_on_off.ino
@@ -0,0 +1,21 @@
+int relay1=8;
+int relay2=9;
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(relay1,OUTPUT);
+  pinMode(relay2,OUTPUT);
+  digitalWrite(relay1,LOW);
+  digitalWrite(relay2,LOW);
+  delay(200);
+}
+void loop()
+{
+  digitalWrite(relay1,HIGH);
+  delay(1000);
+  digitalWrite(relay1,LOW);
+  digitalWrite(relay2,HIGH);
+  delay(1000);
+  digitalWrite(relay2,LOW);
+}
